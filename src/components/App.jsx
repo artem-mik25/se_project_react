560fdbabdee0df101137b1d615ec9b2ba11b354b
artem-mik25 <mikhaylov25.artem@gmail.com>
2025-05-13 23:02:15 -0400

    started project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import "./Header";
+import Header from"./Header";
 function App() {
 
   return (
     <>
-      <h1>Hello Vite!</h1>
+      <>Header/>
       <p>This is your first Vite Project</p>
     </>
   )
